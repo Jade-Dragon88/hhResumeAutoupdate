b9161546279abb64b5b7a64313aaab7c3be77aeb
JadeDragon <OlegOvNiNo@mail.ru>
2022-10-29 18:27:48 +0300

    Оптимизация bg.js
    Рефакторинг кода, избавление от лишних
    вызовов, переменных и др.

--- bg.js
+++ bg.js
@@ -31,22 +31,45 @@
   hhTab.then(hhTabIsCompleted, logError) */
 }
 
-function recolorResumeName(resumeName) {
-  // if(!resumeName) {
-  //   console.log('!!! ОШИБКА: не указано резюме !!!');
-  //   return;
-  // }
-  // let resume = resumeName;
-  // let r, g, b, color;
-  // window.location.reload();
+function tabReload() {
+  window.location.reload();
+}
 
+function logError(error) {
+  // функция отображения ошибки
+  console.error(`Error: ${error}`);
+}
+
+function getTab(url) {
+  return chrome.tabs.query({
+    //ищем страницу резюме среди вкладок браузера
+    url: url,
+  });
+}
+
+function executingScript(target, func, args = null) {
+  chrome.scripting.executeScript({
+    // и инкапсулируем в страницу код tabReload
+    target: { tabId: target[0].id },
+    function: func,
+    args: [args],
+  });
+}
+
+function recolorResumeName(resumeName) {
+  function randomNumber(max) {
+    // Случайное число [0;max-1]
+    return Math.floor(Math.random() * max);
+  }
   let recolorResumeNameInterval = setInterval(() => {
-    // интервал на 5 сек
+    // let recolorResumeNameTimeout = setTimeout(() => {
+    // таймер на 3 сек
     // console.log(`resumeName = ${resumeName}`);
-    let r = Math.floor(Math.random() * 256),
-      g = Math.floor(Math.random() * 256),
-      b = Math.floor(Math.random() * 256),
-      color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+    let color =
+      '#' +
+      `${`00${(51 + randomNumber(205)).toString(16)}`.slice(-2)}` +
+      `${`00${(51 + randomNumber(205)).toString(16)}`.slice(-2)}` +
+      `${`00${(51 + randomNumber(205)).toString(16)}`.slice(-2)}`;
     // let resume = document.querySelector(`[data-qa-title="${resumeName}"] span`);
     let resume = document.querySelector(`[data-qa-title="${resumeName}"]`);
     resume
@@ -56,25 +79,21 @@ function recolorResumeName(resumeName) {
       : null;
     upDateBtn
       ? (upDateBtn.style.cssText = `
-          border-bottom: 3px dashed ${color};
+          border-bottom: 3px dashed ${color}; 
           padding-bottom: 0`)
-      : null;
+      : // upDateBtn.click(); tabReload()
+        null;
     console.log(`color = ${color}`);
     // console.log('resume = ');
     // console.log(resume);
-  }, 1000);
-}
-
-function tabReload() {
-  window.location.reload();
+  }, 3000);
 }
 
 function startSmallInterval() {
-  // функция малого интервала; проверяет имя, страницу и её статус
-
-  let validator = setInterval(async () => {
+  // функция малого интервала; проверяет имя, присутствие страницы и её статус загрузки
+  let validator = setInterval(() => {
     // малый интервал на 5 сек
-    await chrome.storage.local.get(console.log); // отобразить local storage из ServiceWorker
+    // chrome.storage.local.get(console.log); // отобразить local storage из ServiceWorker
     let name; // внутр. переменная для сохранения имени резюме
 
     let resumeName = chrome.storage.local.get(['resumeName']); //вытаскиваем из SWLS название резюме
@@ -84,8 +103,8 @@ function startSmallInterval() {
         if (!result.resumeName) {
           // если в SWLS названия резюме нет, то
           console.log('ИМЯ РЕЗЮМЕ НЕ УСТАНОВЛЕНО');
-          return;
-        } // ничего не делай
+          return; // ничего не делай
+        }
         console.log('ИМЯ РЕЗЮМЕ УКАЗАНО');
         hhTabIsOpened(); // иначе выполни проверку страницы
       },
@@ -94,15 +113,12 @@ function startSmallInterval() {
 
     function hhTabIsOpened() {
       // функция проверки присутствия страницы среди вкладок браузера
-      let hhTab = chrome.tabs.query({
-        //ищем страницу резюме среди вкладок браузера
-        url: '*://nn.hh.ru/applicant/resumes*',
-      });
+      let hhTab = getTab('*://nn.hh.ru/applicant/resumes*');
       hhTab.then(
         // если есть result запусти проверку полной загрузки страницы
         (result) => {
           console.log('СТРАНИЦА ОТКРЫТА');
-          hhTabIsCompleted(result[0]);
+          hhTabIsCompleted(result);
         },
         (error) => logError(error)
       );
@@ -110,72 +126,40 @@ function startSmallInterval() {
 
     function hhTabIsCompleted(hhTab) {
       // функция проверки полной загрузки страницы
-      if (hhTab.status != 'complete') {
+      if (hhTab[0].status != 'complete') {
         console.log('СТРАНИЦА ВСЕ ЕЩЁ ГРУЗИТСЯ');
         return;
       }
       // console.log('name: ' + name);
-      console.log('СТРАНИЦА ЗАГРУЖЕНА');
       clearInterval(validator); // останавливаем малый интервал validator
-      console.log('validator ОСТАНОВЛЕН');
-      scriptExecuter(name, recolorResumeName); // запуск функции инкапсуляции
+      executingScript(hhTab, recolorResumeName, name); // запуск функции инкапсуляции
+      startBigInterval(); // запуск большого интервала
+
+      console.log(`СТРАНИЦА ЗАГРУЖЕНА
+validator ОСТАНОВЛЕН
+START_BIG_INTERVAL запущен`);
       // chrome.storage.local.clear(); // очищаем ServiceWorker LocalStorage, удаляем имя резюме
       // console.log('УДАЛИЛИ ИМЯ РЕЗЮМЕ');
-      startBigInterval(); // запуск большого интервала
     }
   }, 5000);
 }
 
-function scriptExecuter(resumeName, func) {
-  // функция инкапсуляции кода на страницу
-  let hhTab = chrome.tabs.query({
-    //ищем страницу резюме среди вкладок браузера
-    url: '*://nn.hh.ru/applicant/resumes*',
-  });
-  hhTab.then(
-    (result) => {
-      chrome.scripting.executeScript({
-        // и инкапсулируем в страницу код recolorResumeName
-        target: { tabId: result[0].id },
-        function: func,
-        args: [resumeName],
-      });
-    },
-    (error) => logError(error)
-  );
-}
-
 function startBigInterval() {
-  console.log('START_BIG_INTERVAL запущен');
+  // console.log('');
   let bigInterval = setTimeout(() => {
     // большой интервал на 60 секунд
-
-    let hhTab = chrome.tabs.query({
-      url: '*://nn.hh.ru/applicant/resumes*',
-    });
-    hhTab.then(hhTabReload, logError);
-    console.log('КОМАНДА НА ПЕРЕЗАГРУЗКУ ОТПРАВЛЕНА');
+    let hhTab = getTab('*://nn.hh.ru/applicant/resumes*'); // снова проверяем присутствие страницы
+    hhTab.then(
+      (result) => executingScript(result, tabReload),
+      (error) => logError(error)
+    );
     clearTimeout(bigInterval);
-    console.log('BIG_INTERVAL ОСТАНОВЛЕН');
     startSmallInterval();
-    console.log('START_SMALL_INTERVAL ЗАПУЩЕН');
-    console.log('********************************************************');
-  }, 60000);
-}
-
-function hhTabReload(hhTab) {
-  // console.dir(hhTab);
-  chrome.scripting.executeScript({
-    // и инкапсулируем в страницу код tabReload
-    target: { tabId: hhTab[0].id },
-    function: tabReload,
-    // args:[resumeName]
-  });
-}
-
-function logError(error) {
-  // функция отображения ошибки
-  console.error(`Error: ${error}`);
+    console.log(`КОМАНДА НА ПЕРЕЗАГРУЗКУ ОТПРАВЛЕНА
+BIG_INTERVAL ОСТАНОВЛЕН
+START_SMALL_INTERVAL ЗАПУЩЕН
+********************************************************`);
+  }, /*4 * 60 * 60000 + */ 60000);
 }
 
 startSmallInterval();
