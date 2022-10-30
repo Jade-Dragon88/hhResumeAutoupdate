f039b7b76217c1e672cab646f7edeb74ca15e148
JadeDragon <OlegOvNiNo@mail.ru>
2022-10-30 19:05:13 +0300

    Оптимизация my_content.js
    Оптимизировал скрипт при переключении состояния
    start/stop. Добавил ф-цию changeBtn

--- my_content.js
+++ my_content.js
@@ -2,30 +2,30 @@ let resumeName; // название резюме
 const input = document.querySelector('#resumeName'); // поле для ввода названия резюме
 const btnStartStop = document.querySelector('#btnStartStop'); // кнопка для запуска/остановки скрипта
 
+function changeBtn(btnText) {
+  if (btnText == 'START') {
+    chrome.storage.local.set({ resumeName: resumeName }); // записываем в localStorage название резюме
+    btnStartStop.innerText = 'STOP';
+  }
+  if (btnText == 'STOP') {
+    chrome.storage.local.remove(['resumeName']); // удаляем из localStorage название резюме
+    [resumeName, input.value] = [null, null]; // обнуляем resumeName и значение input
+    btnStartStop.innerText = 'START';
+  }
+  chrome.storage.local.get(console.log);
+  // console.dir(chrome.storage);
+  console.dir(input);
+  btnStartStop.classList.toggle('btn-success');
+  btnStartStop.classList.toggle('btn-warning');
+}
+
 input.addEventListener('change', (e) => {
   e.preventDefault();
   resumeName = input.value; // сохраняем в переменную вводимое название резюме
 });
 
-btnStartStop.addEventListener(
-  'click',
-  /* async */ (e) => {
-    e.preventDefault();
-    if (e.target == btnStartStop) {
-      chrome.storage.local.set({ resumeName: resumeName }); // записываем в localStorage название резюме
-      console.dir(btnStartStop.innerText);
-      switch (btnStartStop.innerText) {
-        case 'START':
-          btnStartStop.innerText = 'STOP';
-          btnStartStop.classList.remove('btn-success');
-          btnStartStop.classList.add('btn-warning');
-          break;
-        case 'STOP':
-          btnStartStop.innerText = 'START';
-          btnStartStop.classList.remove('btn-warning');
-          btnStartStop.classList.add('btn-success');
-          break;
-      }
-    }
-  }
-);
+btnStartStop.addEventListener('click', (e) => {
+  e.preventDefault();
+  let btnText = btnStartStop.innerText;
+  e.target == btnStartStop ? changeBtn(btnText) : null;
+});
